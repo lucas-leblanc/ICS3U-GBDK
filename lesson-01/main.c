64f1c15b7d38fd1edbb5ec046dbfb304c05e9681
Lucas LeBlanc <lucas.leblanc@mths.ca>
2023-01-18 16:18:59 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Lucas LeBlanc
+//
+// Created by: Lucas LeBlanc
+// Created on: Jan 2023
+// This program is the Space Aliens game for the Gameboy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main(){
+    printf("Hello, World!");
+}
